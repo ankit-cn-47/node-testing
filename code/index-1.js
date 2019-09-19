1b25ff826126d707752e826b3c2b8f84a2d51184
ankit-cn-47 <ankit.codenamefourty7@gmail.com>
2019-09-19 16:13:31 +0530

    first meaningful commit

---
+++ code/index-1.js
@@ -0,0 +1,12 @@
+module.exports.fizzbuzz = function(value){
+    if(value%3 == 0){
+        if(value%5 == 0)
+            return 'FizzBuzz';
+        return 'Fizz';
+    }
+
+    if(value%5 == 0)
+        return 'Buzz';
+
+    return value.toString();
+}
